f15d418d1c74f43fe2e6ba7e9073c9c891108d09
Enzo <enzojatip@gmail.com>
2025-11-05 14:08:37 -0300

    Corregimos fondo y agregamos efecto parallax on mouse move

--- PortfolioEnzo/js/app.js
+++ PortfolioEnzo/js/app.js
@@ -7,3 +7,23 @@ document.addEventListener("DOMContentLoaded", () => {
       navLinks.classList.toggle('active');
     });
   });
+
+ const seccion = document.querySelector(".seccion-presentacion");
+  const seccionWidth = window.innerWidth;
+  const seccionHeight = window.innerHeight;
+
+  document.addEventListener("mousemove", (event) => {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+
+    const xOffset = (mouseX / seccionWidth) * 2;
+    const yOffset = (mouseY / seccionHeight) * 2;
+
+    const movimientoMaximo = 10;
+
+    seccion.style.backgroundPosition = `${50 - xOffset * movimientoMaximo}% ${50 - yOffset * movimientoMaximo}%`;
+  });
+
+  seccion.addEventListener("mouseleave", () => {
+    seccion.style.backgroundPosition = "50% 50%";
+  });
